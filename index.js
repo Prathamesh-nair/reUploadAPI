b36ceed945e96770bb3204ce2340763ae5580a9e
Prathamesh Nair <prathamesh.nair@angelbroking.com>
2023-09-21 15:27:25 +0530

    added bearerToken authentication.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 const express = require("express");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const logger = require("./logger");
 const app = express();
 const bodyParser = require("body-parser");
@@ -11,25 +11,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//   const headerToken = req.headers["authorization"];
-//   if (typeof headerToken !== "undefined") {
-//     const token = headerToken.split(" ")[1];
-//     const bearerToken = jwt.decode(token);
-//     // console.log(bearerToken);
-//     if (bearerToken.agentData.exp >= dateNow.getTime() / 1000) {
-//       if (bearerToken.agentData.role !== "38") {
-//         next();
-//       } else {
-//         res.status(401).json({ message: `User not Authenticated.` });
-//       }
-//     } else {
-//       res.status(400).json({ message: `Token is expired.` });
-//     }
-//   } else {
-//     res.status(401).json({ message: `Token Not Found.` });
-//   }
-// });
+app.use((req, res, next) => {
+  const headerToken = req.headers["authorization"];
+  if (typeof headerToken !== "undefined") {
+    const token = headerToken.split(" ")[1];
+    const bearerToken = jwt.decode(token);
+    req.bearerToken = bearerToken;
+    if (bearerToken.agentData.role !== "38") {
+      next();
+    } else {
+      res.status(401).json({ message: `User not Authenticated.` });
+    }
+  } else {
+    res.status(401).json({ message: `Token Not Found.` });
+  }
+});
 
 app.get("/application/:id", (req, res) => {
   client.query(
@@ -46,29 +42,27 @@ app.get("/application/:id", (req, res) => {
 });
 
 app.post("/application/:id", (req, res) => {
-  const { Ecode, requestID } = req.body;
+  const { requestID } = req.body;
 
-  if (!Ecode || !requestID) {
-    res.status(401).json({ message: `Kindly provide Ecode and Request ID` });
+  if (!requestID) {
+    res.status(401).json({ message: `Kindly provide Request ID` });
   }
   client.query(
     `
-  update scrutiny set status = 'pending' , last_update_ts = CURRENT_TIMESTAMP where app_number = '${req.params.id}';
-  `,
+    update scrutiny set status = 'pending' , last_update_ts = CURRENT_TIMESTAMP, last_update_source = 'techops_api' where app_number = '${req.params.id}';
+    `,
     (err, result) => {
       if (!err) {
         res
           .status(200)
           .json({ message: `${req.params.id} updated Successfully` });
         logger.info(
-          `${req.params.id} has been updated by ${Ecode} against request id:${requestID}`
+          `${req.params.id} has been updated by ${req.bearerToken.agentData.id}: ${req.bearerToken.agentData.name} against the request id: ${requestID}`
         );
       } else {
         res.status(500).json({
           message: `There is some technical issue. Please try again.`,
         });
-        // console.log(error);
-        // logger.error(``)
       }
     }
   );
